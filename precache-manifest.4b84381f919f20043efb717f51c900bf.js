adb811b926b671407b72aedbeebd185190d8e4f0
Lawrence <jovenlawrencegersaniba.fcu@gmail.com>
2019-11-24 11:59:37 +0800

    add restore file

---
+++ precache-manifest.4b84381f919f20043efb717f51c900bf.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ace14630cffb757add7d11bd17efe49",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7379823361e8689c26d5",
+    "url": "/portfolio/static/css/main.5058b762.chunk.css"
+  },
+  {
+    "revision": "01a7811e2ddda94dcd3d",
+    "url": "/portfolio/static/js/2.16fc1cf2.chunk.js"
+  },
+  {
+    "revision": "7379823361e8689c26d5",
+    "url": "/portfolio/static/js/main.ae92f717.chunk.js"
+  },
+  {
+    "revision": "6b26cb3bc1b9147ba086",
+    "url": "/portfolio/static/js/runtime-main.86a05020.js"
+  },
+  {
+    "revision": "794c3db378a1713f92dc080de306d5d0",
+    "url": "/portfolio/static/media/android-studio-logo.794c3db3.png"
+  },
+  {
+    "revision": "d445a19413973885b93b365911b88a4c",
+    "url": "/portfolio/static/media/e_order.d445a194.png"
+  },
+  {
+    "revision": "fefecad8fd64060a06255eee1d889d7a",
+    "url": "/portfolio/static/media/odoo-logo.fefecad8.png"
+  },
+  {
+    "revision": "42ab5db1a448bfd66d3e569b3559211d",
+    "url": "/portfolio/static/media/portfolio.42ab5db1.png"
+  },
+  {
+    "revision": "9125a3da0e804fc6b7b42693962d9798",
+    "url": "/portfolio/static/media/portrait.9125a3da.jpg"
+  },
+  {
+    "revision": "8bac5bb613c05f8cfc6ba4570ca8453c",
+    "url": "/portfolio/static/media/postgresql-logo.8bac5bb6.png"
+  },
+  {
+    "revision": "6c5b56817662254ad278346ccb0c7ccd",
+    "url": "/portfolio/static/media/todo.6c5b5681.png"
+  }
+]);
