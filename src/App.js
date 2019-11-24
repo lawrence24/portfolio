7e76e698fda8c5c23dbe7a2feb6ec4700349c100
Lawrence <jovenlawrencegersaniba.fcu@gmail.com>
2019-11-24 12:51:19 +0800

    a

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export default class App extends Component {
       <div>
         <Nav />
         <Switch>
-          <Route path={process.env.PUBLIC_URL + "/"} exact component={Home} />
+          <Route path={"/" + process.env.PUBLIC_URL} exact component={Home} />
           <Route path="/about" component={About} />
           <Route path="/contact" component={Contact} />
         </Switch>
